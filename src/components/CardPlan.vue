5c0feb9c5c8fc6059cc957c99e61be2c26cec2ff
Caio Henrique <caiohenrique@jeraapps.com>
2021-09-02 18:27:01 -0400

    [NEW] card plan, editor config, routes

---
+++ src/components/CardPlan.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <h1>Guardian</h1>
+    <p>What You’ll Get</p>
+    <div class="checks">
+      <div>
+        <span>Edit up to 10 hours of podcast audio files.</span>
+      </div>
+      <div>
+        <span>Set your own CoreLoop Page</span>
+      </div>
+    </div>
+    <h1>Free Forever</h1>
+    <button>Choose</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardPlan',
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <card-plan />
+  </div>
+</template>
+
+<script>
+import CardPlan from '../components/CardPlan.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    CardPlan,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
